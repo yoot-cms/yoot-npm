4d75f7eb87a5030a85ebd3a65e98d9527a5d1e41
TheWisePigeon <zozozozeph@gmail.com>
2023-07-17 21:47:21 +0000

    feat: get list of entities

--- index.ts
+++ index.ts
@@ -1,3 +1,32 @@
-export function init( _key:string ){
-    return true
+type Entity = {
+    name: string,
+    project: string,
+    schema: Record<string, string>,
+    trashed: boolean
+}
+
+export class Yoot {
+    api_key: string
+
+    constructor(api_key: string) {
+        this.api_key = api_key
+    }
+
+    async get_entities(): Promise<Entity[]> {
+        try {
+            const response = await fetch(
+                "http://localhost:5000/entities",
+                {
+                    headers: {
+                        Authorization: this.api_key
+                    }
+                }
+            )
+            if(response.status===401) throw Error("Invalid api key")
+            const entities = await response.json()
+            return entities as Entity[]
+        } catch (err) {
+            throw err
+        }
+    }
 }
