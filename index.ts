ca4124585b43e3319d202ac0397fc9365c132297
TheWisePigeon <zozozozeph@gmail.com>
2023-07-30 11:38:43 +0000

    ref: minor updates

--- index.ts
+++ index.ts
@@ -1,32 +1,38 @@
 type Entity = {
-    name: string,
-    project: string,
-    schema: Record<string, string>,
-    trashed: boolean
+  name: string,
+  project: string,
+  schema: Record<string, string>,
+  trashed: boolean
+}
+
+type Entry = {
+  id: string,
+  entity: string,
+  value: Record<string, string | number | boolean>
 }
 
 export class Yoot {
-    api_key: string
+  api_key: string
 
-    constructor(api_key: string) {
-        this.api_key = api_key
-    }
+  constructor(api_key: string) {
+    this.api_key = api_key
+  }
 
-    async get_entities(): Promise<Entity[]> {
-        try {
-            const response = await fetch(
-                "http://localhost:5000/entities",
-                {
-                    headers: {
-                        Authorization: this.api_key
-                    }
-                }
-            )
-            if(response.status===401) throw Error("Invalid api key")
-            const entities = await response.json()
-            return entities as Entity[]
-        } catch (err) {
-            throw err
+  async get_entities(): Promise<Entity[]> {
+    const response = await fetch(
+      "http://localhost:5000/entities",
+      {
+        headers: {
+          Authorization: this.api_key
         }
+      }
+    )
+    if (response.status !== 200) {
+      if (response.status === 500) throw Error("ERR_INTERNAL_SERVER")
+      const error = await response.text()
+      throw Error(error)
     }
+    const entities = await response.json()
+    return entities as Entity[]
+  }
 }
